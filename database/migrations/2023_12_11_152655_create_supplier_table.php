c993f8f6ddc8fdebd84fff6c9a6695b1a224475f
faishal280803 <faishalluqman41@gmail.com>
2023-12-11 22:37:57 +0700

    add database migration

--- database/migrations/2023_12_11_152655_create_supplier_table.php
+++ database/migrations/2023_12_11_152655_create_supplier_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('supplier', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_lengkap');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nama_supplier');
+            $table->string('alamat_supplier');
+            $table->string('no_telepon');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('supplier');
     }
 };
---
+++ database/migrations/2023_12_11_152730_create_produk_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang');
+            $table->unsignedBigInteger('id_kategori');
+            $table->unsignedBigInteger('id_cabang');
+            $table->unsignedBigInteger('id_supplier');
+            $table->string('harga_jual');
+            $table->string('harga_beli');
+            $table->string('stok');
+            $table->timestamps();
+
+            $table->foreign('id_kategori')->references('id')->on('kategori')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('id_cabang')->references('id')->on('cabang')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('id_supplier')->references('id')->on('supplier')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produk');
+    }
+};
